99dec8c8881cec06bf35f67ea34f606fb637a474
Bruno Cavalcante Pires <63984897+Hubbleq@users.noreply.github.com>
2025-06-25 02:45:36 +0000

    DELETE /accout/{id} GET /account/{id} com erro 404

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.routers import accounts, transactions, categories
 from app.database import create_db_and_tables
+from app.routers import accounts, transactions, categories, reports
+from app.routers import reports
+
 
 app = FastAPI(title="Finance API")
+
 
 @app.on_event("startup")
 def on_startup():
@@ -11,6 +15,8 @@ def on_startup():
 app.include_router(accounts.router)
 app.include_router(transactions.router)
 app.include_router(categories.router)
+app.include_router(reports.router)
+app.include_router(reports.router)
 
 @app.get("/")
 def read_root():
