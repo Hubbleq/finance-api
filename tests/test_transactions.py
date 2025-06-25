d1ba9727a1c35c2fa17870a0a185dc462a5b90e9
Bruno Cavalcante Pires <63984897+Hubbleq@users.noreply.github.com>
2025-06-25 03:31:18 +0000

    Adding Test_transcations.py

---
+++ tests/test_transactions.py
@@ -0,0 +1,19 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+from sqlmodel import SQLModel
+from app.database import engine
+
+@pytest.fixture(scope="module")
+def test_db():
+    SQLModel.metadata.create_all(bind=engine)
+    yield
+    SQLModel.metadata.drop_all(bind=engine)
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
+
+def test_get_transactions(client):
+    response = client.get("/transactions/")  # ajuste se necessário
+    assert response.status_code == 200
